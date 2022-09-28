cbef9cc5167a13cfdbc29f3b637176db207de820
I do <ido_p_a@hotmail.com>
2022-09-28 17:28:00 +0300

    updated demo , practice

---
+++ DOTNET/Basics/Exceptions/Demo1-MultiCatch/Program.cs
@@ -0,0 +1,15 @@
+﻿try{
+    Console.WriteLine("t1");
+    throw new ApplicationException("error");
+}
+catch(ArgumentException ae ){
+    Console.WriteLine("c1 " + ae.Message);
+}
+catch (Exception e)
+{
+    Console.WriteLine("c1 e");
+}
+finally{
+    Console.WriteLine("f1");
+}
+Console.WriteLine("af");
---
+++ DOTNET/Basics/Exceptions/Demo2-Nested/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Main start");
+try
+{
+     F1();
+     F3();
+     F2();
+}
+catch (System.Exception e)
+{
+    Console.WriteLine($"Main function exception catch {e.Message}");
+}
+Console.WriteLine("Main end");
+///////////////////////////////
+void F1()
+{
+   Console.WriteLine("f1");
+}
+//
+void F2()
+{
+    Console.WriteLine("f2 start");
+    int[] a = {2,4,5};
+    Console.WriteLine(a[444]);
+    Console.WriteLine("f2 end");
+}
+//
+void F3(){
+    try
+    {
+        Console.WriteLine("f3 start try");
+        int[] a = {2,4,5};
+        Console.WriteLine(a[444]);
+        Console.WriteLine("f3 end try");
+    }
+    catch (System.Exception e)
+    {
+        Console.WriteLine("f3 exception");
+    }
+}
