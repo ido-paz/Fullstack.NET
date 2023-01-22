06ede73638df968916d2ab9f25d9dd87b5b7628a
I do <ido_p_a@hotmail.com>
2023-01-22 17:31:40 +0200

    updated practice

---
+++ DOTNET/DataAccess/ADO.NET/Todos/Todos_SqlCommand_StoredProcedure_Console/IItemsCRUD.cs
@@ -0,0 +1,22 @@
+﻿public interface IItemsCRUD
+{
+    #region Read / Get
+    public IEnumerable<Item> GetAllItems();
+    public Item GetItem(string title);
+    #endregion
+
+    #region Update 
+    public int UpdateItem(Item item);
+    #endregion
+
+    #region Create / Add / Insert
+    public int InsertItem(Item item);
+    #endregion
+
+    #region Remove / Delete
+    public int DeleteItem(Item item);
+    public int DeleteItem(int id);
+    public int DeleteItem(string title);
+    #endregion
+
+}
---
+++ DOTNET/DataAccess/ADO.NET/Todos/Todos_SqlCommand_Transaction_Console/Item.cs
@@ -0,0 +1,25 @@
+﻿public class Item
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public bool IsCompleted { get; set; }
+
+    public Item() { }
+    public Item(object[] values)
+    {
+        if (values != null)
+        {
+            if (values.Length == 3)
+            {
+                Id = (int)values[0];
+                Title = values[1].ToString();
+                IsCompleted = (bool)values[2];
+            }
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"{Id},{Title},{IsCompleted}";
+    }
+}
