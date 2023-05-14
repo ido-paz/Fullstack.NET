a4b05a01b7ba97eae87070252bc5d12eeb880bb6
I do <ido_p_a@hotmail.com>
2023-05-14 13:18:17 +0300

    moved shop

--- DOTNET/ASP.NET/Shop/ShopClient_Vite/src/contexts/LoginContext/AuthContext.tsx
+++
@@ -1,5 +0,0 @@
-import React, { createContext } from 'react';
-
-const AuthContext = createContext({isLoggedIn : true,userName:null,login:()=>{},logout:()=>{}});
-
-export default AuthContext;
---
+++ General/Shop/Client/ShopReactVite/src/publicPages/Home.jsx
@@ -0,0 +1,14 @@
+const Home = (props) => {
+  return (
+    <>
+      <h1>        
+        Home</h1>
+      <p>
+        Welcome to our e-shop! We’re excited to have you here. Browse our
+       receive exclusive offers and updates. Happy shopping!
+      </p>
+    </>
+  );
+};
+
+export default Home;
